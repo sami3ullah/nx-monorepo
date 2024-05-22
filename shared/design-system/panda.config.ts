cb3b4b670878bb5d57e3d1facd4e752a305fe2fa
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2024-05-22 22:05:46 +0200

    chore: split preset entrypoint so that esbuild doesnt try to bundle components when loading the panda.config

--- shared/design-system/panda.config.ts
+++ shared/design-system/panda.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from '@pandacss/dev';
-import { propiflyPreset } from './src';
+import { propiflyPreset } from './src/preset';
 import { createPreset } from '@park-ui/panda-preset';
 
 export default defineConfig({
