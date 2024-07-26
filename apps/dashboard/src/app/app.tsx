881f7d1640351d867712dcc7ebfb723476eb1cc2
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2024-07-26 13:59:54 +0200

    chore: update panda + fix design-system imports + import from design-system module (just like in importMap)

--- apps/dashboard/src/app/app.tsx
+++ apps/dashboard/src/app/app.tsx
@@ -1,7 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Button } from '@nx-monorepo/design-system';
-import { css } from '../styled-system/css';
-import { VStack, Box } from '../styled-system/jsx';
+// TODO: not sure why I need to use `/index.js` here, NX is doing something weird
+import { css } from '@nx-monorepo/design-system/css/index.js';
+import { VStack, Box } from '@nx-monorepo/design-system/jsx/index.js';
 
 export function App() {
   return (
@@ -12,6 +13,7 @@ export function App() {
         borderRadius="50%"
         bg="nice-yellow"
         animation="bounce 1s infinite"
+        animationName="skeleton-pulse"
       ></Box>
       <Box className={css({ bg: 'red.300' })}>
         <Button>Primary</Button>
--- shared/design-system/src/lib/components/button/button.component.ts
+++ shared/design-system/src/lib/components/button/button.component.ts
@@ -1,7 +1,7 @@
 import { ark } from '@ark-ui/react/factory';
 import type { ComponentProps } from 'react';
-import { styled } from '../../../styled-system/jsx';
-import { buttonRecipe } from '../../../styled-system/recipes';
+import { styled } from '../../../../styled-system/jsx';
+import { buttonRecipe } from '../../../../styled-system/recipes';
 
 export const Button = styled(ark.button, buttonRecipe);
 export interface ButtonProps extends ComponentProps<typeof Button> {}
