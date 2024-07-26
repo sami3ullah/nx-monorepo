47171088cb1ef05349d48dffccb44eb910170d0b
Muhammad Samiullah <msami398@gmail.com>
2024-07-26 22:09:00 +0500

    added animations inside of pandacss

--- apps/dashboard/src/app/app.tsx
+++ apps/dashboard/src/app/app.tsx
@@ -1,8 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Button } from '@nx-monorepo/design-system';
 // TODO: not sure why I need to use `/index.js` here, NX is doing something weird
-import { css } from '@nx-monorepo/design-system/css/index.js';
-import { VStack, Box } from '@nx-monorepo/design-system/jsx/index.js';
+import { css } from '../styled-system/css/css';
+import { VStack, Box } from '../styled-system/jsx';
 
 export function App() {
   return (
@@ -12,8 +12,7 @@ export function App() {
         w="60px"
         borderRadius="50%"
         bg="nice-yellow"
-        animation="bounce 1s infinite"
-        animationName="skeleton-pulse"
+        animation="bounce"
       ></Box>
       <Box className={css({ bg: 'red.300' })}>
         <Button>Primary</Button>
